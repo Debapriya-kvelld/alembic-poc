a35c216af7277c30bc11cbe26af292b1cbc00e8a
Debapriya-kvelld <debapriya.das@kvelld.co.za>
2025-04-10 02:56:08 +0530

    .

--- app/seed.py
+++ app/seed.py
@@ -1,12 +1,13 @@
+from datetime import datetime
 from app.models import Todo
 from app.database import get_session
 
 def seed():
     session = get_session()
     session.add_all([
-        Todo(title="Learn FastAPI", completed=False),
-        Todo(title="Build Alembic POC", completed=True),
-        Todo(title="Push to GitHub", completed=False)
+        Todo(title="Learn FastAPI", completed=False, due_date=datetime.now(), priority=1),
+        Todo(title="Build Alembic POC", completed=True, due_date=datetime.now(), priority=2),
+        Todo(title="Push to GitHub", completed=False, due_date=datetime.now(), priority=3)
     ])
     session.commit()
     print("✅ Seed data inserted!")
